ee8ae9129ddb90bab6b8802ff2a26b41c54ad2ba
liushuijing <shuijing@shanshu.ai>
2019-06-17 11:39:04 +0800

    1，集成Swagger 2，创建单元测试

--- src/main/java/com/imooc/springboot/HelloController.java
+++ src/main/java/com/imooc/springboot/HelloController.java
@@ -1,13 +1,23 @@
 package com.imooc.springboot;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class HelloController {
 
-    @GetMapping
-    public String hello() {
+    @ApiOperation(value = "Hello Spring Boot 方法")
+    @GetMapping("/")
+    public String hello(@RequestParam(required = false) @ApiParam("名字") String name) {
+        if (name != null && !"".equals(name)) {
+            return "Hello " + name;
+        }
         return "Hello Spring Boot";
     }
 }
---
+++ src/main/java/com/imooc/springboot/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.imooc.springboot;
+
+import com.google.common.base.Predicates;
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger 配置类
+ *
+ * @author 刘水镜
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                        .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                        .paths(PathSelectors.any())
+                        .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Spring Boot实战").description("Spring Boot实战的RESTFul接口文档说明").version("1.0")
+                        .build();
+    }
+}
