3b89bd32d865961b918244c3d2c31fcaca24b630
liushuijinger <545521439@qq.com>
2019-10-13 15:28:03 +0800

    添加事务模块

--- 16-transaction/src/main/java/com/imooc/springboot/transaction/controller/UserController.java
+++ 16-transaction/src/main/java/com/imooc/springboot/transaction/controller/UserController.java
@@ -1,18 +1,12 @@
-package com.imooc.springboot.mybatis.controller;
+package com.imooc.springboot.transaction.controller;
 
-import com.imooc.springboot.mybatis.mapper.UserMapper;
-import com.imooc.springboot.mybatis.model.User;
+
+import com.imooc.springboot.transaction.mapper.UserMapper;
+import com.imooc.springboot.transaction.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
