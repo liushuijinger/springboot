709e50d6a6c1f1326f05c0201199b378b8280025
liushuijing <shuijing@shanshu.ai>
2019-11-10 15:56:45 +0800

    完善注释及优化配置项

--- 20-databasepc/src/main/java/com/imooc/springboot/databasepc/config/DruidConfig.java
+++ 20-databasepc/src/main/java/com/imooc/springboot/databasepc/config/DruidConfig.java
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 
 /**
+ * Druid 监控配置
+ *
  * @author 刘水镜
  * @blog https://liushuijinger.blog.csdn.net
  * @date 2019/11/09
@@ -20,7 +22,46 @@ import java.util.Arrays;
 //@Configuration
 public class DruidConfig {
 
+    /**
+     * 监控信息配置
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean statViewServle() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.setUrlMappings(Arrays.asList("/druid/*"));
 
+        // 白名单
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+
+        // 黑名单 (存在共同时，deny优先于allow)
+        servletRegistrationBean.addInitParameter("deny", "192.168.1.1");
+
+        // 登录查看信息的账号密码
+        servletRegistrationBean.addInitParameter("loginUsername", "druid");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+
+        // 是否能够重置数据
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean statFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+
+        // 添加过滤规则
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        // 不监控静态资源以及 Druid 本身
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+
+    /**
+     * 配置 Spring 监控
+     */
     @Bean
     public DruidStatInterceptor druidStatInterceptor() {
         return new DruidStatInterceptor();
@@ -29,6 +70,8 @@ public class DruidConfig {
     @Bean
     public JdkRegexpMethodPointcut druidStatPointcut() {
         JdkRegexpMethodPointcut druidStatPointcut = new JdkRegexpMethodPointcut();
+
+        // AOP 切点
         String[] patterns = {"com.imooc.springboot.databasepc.controller.*",
                 "com.imooc.springboot.databasepc.service.*",
                 "com.imooc.springboot.databasepc.mapper.*"};
@@ -39,32 +82,5 @@ public class DruidConfig {
     @Bean
     public Advisor druidStatAdvisor() {
         return new DefaultPointcutAdvisor(druidStatPointcut(), druidStatInterceptor());
-    }
-
-    @Bean
-    public ServletRegistrationBean statViewServle() {
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
-        servletRegistrationBean.setServlet(new StatViewServlet());
-        servletRegistrationBean.setUrlMappings(Arrays.asList("/druid/*"));
-        //白名单：
-        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
-        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的即提示:Sorry, you are not permitted to view this page.
-        servletRegistrationBean.addInitParameter("deny", "192.168.1.1");
-        //登录查看信息的账号密码.
-        servletRegistrationBean.addInitParameter("loginUsername", "druid");
-        servletRegistrationBean.addInitParameter("loginPassword", "123456");
-        //是否能够重置数据.
-        servletRegistrationBean.addInitParameter("resetEnable", "false");
-        return servletRegistrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean statFilter() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
-        //添加过滤规则.
-        filterRegistrationBean.addUrlPatterns("/*");
-        //添加不需要忽略的格式信息.
-        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
-        return filterRegistrationBean;
     }
 }
