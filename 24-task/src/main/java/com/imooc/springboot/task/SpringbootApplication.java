07c99a5f19bafcb35367a461a0ad973f25950727
liushuijing <shuijing@shanshu.ai>
2019-12-08 23:40:19 +0800

    定时任务代码

--- 24-task/src/main/java/com/imooc/springboot/task/SpringbootApplication.java
+++ 24-task/src/main/java/com/imooc/springboot/task/SpringbootApplication.java
@@ -3,12 +3,14 @@ package com.imooc.springboot.task;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author 刘水镜
  * @blog https://liushuijinger.blog.csdn.net
  * @date 2019/06/02
  */
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.imooc.springboot.task")
 @MapperScan("com.imooc.springboot.task.mapper")
 public class SpringbootApplication {
