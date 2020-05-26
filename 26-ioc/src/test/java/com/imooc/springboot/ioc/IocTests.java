42749440b06ada0c0573d41e23a5917b0228d4f1
liushuijinger <liushuijinger@163.com>
2020-05-26 23:15:21 +0800

    更新 ioc 示例

--- 26-ioc/src/test/java/com/imooc/springboot/ioc/IocTests.java
+++ 26-ioc/src/test/java/com/imooc/springboot/ioc/IocTests.java
@@ -1,6 +1,10 @@
 package com.imooc.springboot.ioc;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author : 刘水镜
@@ -10,25 +14,58 @@ import org.junit.Test;
 //@RunWith(JUnit4.class)
 public class IocTests {
 
+    // Bean 容器
+    private Map<String, Object> container = new HashMap<>();
+
+    /**
+     * 模拟 Spring 容器初始化
+     */
+    @Before
+    public void init() {
+        Driveable bike = new Bike();
+        container.put("bike", bike);
+
+        Driveable car = new Car();
+        container.put("car", car);
+
+        Driveable train = new Train();
+        container.put("train", train);
+
+        Driveable airPlane = new AirPlane();
+        container.put("airPlane", airPlane);
+
+        Driveable ship = new Ship();
+        container.put("ship", ship);
+    }
+
+    /**
+     * 获取 Bean
+     * @param name
+     * @return
+     */
+    public Object getBean(String name) {
+        return container.get(name);
+    }
+
     @Test
     public void test() {
-        Driveable bike = new Bike();
+        Driveable bike = (Driveable) container.get("bike");
         Person xiaoming = new Person(bike);
         xiaoming.hangOut();
 
-        Driveable car = new Car();
+        Driveable car = (Driveable) container.get("car");
         xiaoming = new Person(car);
         xiaoming.hangOut();
 
-        Driveable train = new Train();
+        Driveable train = (Driveable) container.get("train");
         xiaoming = new Person(train);
         xiaoming.hangOut();
 
-        Driveable airPlane = new AirPlane();
+        Driveable airPlane = (Driveable) container.get("airPlane");
         xiaoming = new Person(airPlane);
         xiaoming.hangOut();
 
-        Driveable ship = new Ship();
+        Driveable ship = (Driveable) container.get("ship");
         xiaoming = new Person(ship);
         xiaoming.hangOut();
     }
